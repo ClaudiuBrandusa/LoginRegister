5de1b36665505b5c2b63ad426ec5713b0da00bc5
Claudiu <47690627+ClaudiuBrandusa@users.noreply.github.com>
2019-10-12 13:28:56 +0300

    Update AdminControl.cs

--- AdminControl.cs
+++ AdminControl.cs
@@ -1,25 +1,27 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace LoginRegister
 {
     public class AdminControl
     {
         string connection;
-        public AdminControl(string Connection)
+        string query;
+        public AdminControl()
         {
-            connection = Connection;
+            connection = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
         }
         public void Ban(string id)
         {
             using (SqlConnection sqlCon = new SqlConnection(connection))
             {
                 sqlCon.Open();
-                string query = "UPDATE accounts SET banned='True' WHERE Id=@Id";
+                query = "UPDATE accounts SET banned='True' WHERE Id=@Id";
                 SqlCommand cmd = new SqlCommand(query, sqlCon);
                 cmd.Parameters.AddWithValue("@Id", id);
                 cmd.ExecuteScalar();
@@ -30,7 +32,7 @@ namespace LoginRegister
             using (SqlConnection sqlCon = new SqlConnection(connection))
             {
                 sqlCon.Open();
-                string query = "UPDATE accounts SET banned='False' WHERE Id=@Id";
+                query = "UPDATE accounts SET banned='False' WHERE Id=@Id";
                 SqlCommand cmd = new SqlCommand(query, sqlCon);
                 cmd.Parameters.AddWithValue("@Id", id);
                 cmd.ExecuteScalar();
@@ -41,7 +43,7 @@ namespace LoginRegister
             using(SqlConnection sqlCon = new SqlConnection(connection))
             {
                 sqlCon.Open();
-                string query = "SELECT theme FROM settings";
+                query = "SELECT theme FROM settings";
                 SqlCommand cmd = new SqlCommand(query, sqlCon);
                 return Convert.ToBoolean(cmd.ExecuteScalar());
             }
@@ -51,9 +53,41 @@ namespace LoginRegister
             using (SqlConnection sqlCon = new SqlConnection(connection))
             {
                 sqlCon.Open();
-                string query = "UPDATE settings SET theme=" + (status?1:0).ToString();
+                query = "UPDATE settings SET theme=@theme";
                 SqlCommand cmd = new SqlCommand(query, sqlCon);
+                cmd.Parameters.AddWithValue("@theme", (status ? 1 : 0).ToString());
                 cmd.ExecuteScalar();
+            }
+        }
+        public bool HasSalt()
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connection))
+            {
+                sqlCon.Open();
+                query = "SELECT CAST(CASE WHEN salt is NULL then 0 else 1 END as BIT) from settings";
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+                return Convert.ToBoolean(cmd.ExecuteScalar());
+            }
+        }
+        public void SetSalt(string salt)
+        {
+            using(SqlConnection sqlCon = new SqlConnection(connection))
+            {
+                sqlCon.Open();
+                query = "UPDATE settings SET salt=@salt";
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+                cmd.Parameters.AddWithValue("@salt", salt);
+                cmd.ExecuteScalar();
+            }
+        }
+        public string GetSalt()
+        {
+            using(SqlConnection sqlCon = new SqlConnection(connection))
+            {
+                sqlCon.Open();
+                query = "SELECT salt FROM settings";
+                SqlCommand cmd = new SqlCommand(query, sqlCon);
+                return cmd.ExecuteScalar().ToString();
             }
         }
     }
